a64e873a16ebf4cca8e3b97fee029064cfe38801
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-08-25 12:00:32 +0400

    #change methods in payment manager

--- Payments.Db/PaymentsManager.cs
+++ Payments.Db/PaymentsManager.cs
@@ -12,21 +12,43 @@ public class PaymentsManager
     }
 
     /// <summary>
+    /// Метод для получения пользователя по userId
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    public User GetUser(int userId)
+    {
+        return _paymentsDbContext.Users.FirstOrDefault(x => x.Id == userId);
+    }
+
+    /// <summary>
+    /// Метод для создания пользователя по userId
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    public User GetOrCreateUser(int userId)
+    {
+        var user = GetUser(userId);
+        if (user == null)
+        {
+            user = new User()
+            {
+                Id = userId,
+                Name = "newUser " + DateTime.Now
+            };
+            SaveUser(user);
+        }
+
+        return user;
+    }
+
+    /// <summary>
     /// Метод который возвращает через менеджер-обертку пользователей
     /// </summary>
     /// <returns></returns>
     public User[] GetUsers()
     {
         return _paymentsDbContext.Users.ToArray();
-    }
-
-    /// <summary>
-    /// Метод который возвращает через менеджер-обертку транзакции по userId
-    /// </summary>
-    /// <returns></returns>
-    public Transaction[] GetTransactionsByUserId(int userId)
-    {
-        return _paymentsDbContext.Transactions.Where(x => x.UserId == userId).ToArray();
     }
 
     /// <summary>
@@ -42,6 +64,15 @@ public class PaymentsManager
     }
 
     /// <summary>
+    /// Метод который возвращает через менеджер-обертку транзакции по userId
+    /// </summary>
+    /// <returns></returns>
+    public Transaction[] GetTransactionsByUserId(int userId)
+    {
+        return _paymentsDbContext.Transactions.Where(x => x.UserId == userId).ToArray();
+    }
+
+    /// <summary>
     /// Метод для добавления трансакции
     /// </summary>
     /// <param name="transaction"></param>
@@ -49,7 +80,7 @@ public class PaymentsManager
     public Transaction CreateTransaction(int userId)
     {
         GetOrCreateUser(userId);
-        
+
         Transaction transaction = new Transaction()
         {
             UserId = userId,
@@ -59,26 +90,5 @@ public class PaymentsManager
         _paymentsDbContext.Transactions.Add(transaction);
         _paymentsDbContext.SaveChanges();
         return transaction;
-    }
-
-    public User GetUser(int userId)
-    {
-        return _paymentsDbContext.Users.FirstOrDefault(x => x.Id == userId);
-    }
-
-    public User GetOrCreateUser(int userId)
-    {
-        var user = GetUser(userId);
-        if (user == null)
-        {
-            user = new User()
-            {
-                Id = userId,
-                Name = "newUser " + DateTime.Now
-            };
-            SaveUser(user);
-        }
-
-        return user;
     }
 }
